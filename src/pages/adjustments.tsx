c10de1374b7c46a00afc1a8e0cd4be851b225cf1
isaque.s <isaque.error404@gmail.com>
2022-11-28 15:23:46 -0300

    feat: create new pages

---
+++ src/pages/adjustments.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "../components/template/Layout";
+
+export default function Adjustments() {
+  return (
+    <div>
+      <Layout
+        title="Ajustes e Configurações"
+        subtitle="Personalize o sistema por aqui"
+      >
+        <h2>Conteúdo</h2>
+      </Layout>
+    </div>
+  );
+}
---
+++ src/pages/notification.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/template/Layout";
+
+export default function Notification() {
+  return (
+    <div>
+      <Layout title="Notificações" subtitle="Veja aqui suas Notificações">
+        <h2>Conteúdo</h2>
+      </Layout>
+    </div>
+  );
+}
