5d2f235f09fbfba32d0bb8fb30d8a6fa0b47bf28
isaque.s <isaque.error404@gmail.com>
2022-11-29 10:20:13 -0300

    feat(heading): add ButtonTheme, useAppData and types

--- src/components/template/Heading.tsx
+++ src/components/template/Heading.tsx
@@ -1,3 +1,5 @@
+import useAppData from "../../data/hook/useAppData";
+import { ButtonTheme } from "./ButtonTheme";
 import { Title } from "./Title";
 
 type HeadingProps = {
@@ -6,9 +8,13 @@ type HeadingProps = {
 };
 
 export function Heading(props: HeadingProps) {
+  const { theme, changeTheme } = useAppData();
   return (
-    <div>
+    <div className="flex">
       <Title title={props.title} subtitle={props.subtitle} />
+      <div className="flex flex-grow justify-end">
+        <ButtonTheme theme={theme} changeTheme={changeTheme} />
+      </div>
     </div>
   );
 }
