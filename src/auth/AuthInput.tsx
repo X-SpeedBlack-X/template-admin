beaf9edf0a1e453b4d923ea0941f8b0a3b125f52
isaque.s <isaque.error404@gmail.com>
2022-11-30 12:56:03 -0300

    style(authinput): add new styles

--- src/auth/AuthInput.tsx
+++ src/auth/AuthInput.tsx
@@ -8,13 +8,14 @@ type AuthInputProps = {
 };
 export function AuthInput(props: AuthInputProps) {
   return props.notRenderWhen ? null : (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-4">
       <label htmlFor="">{props.label}</label>
       <input
         type={props.typeInput ?? "text"}
         value={props.value}
         onChange={(e) => props.changeValue?.(e.target.value)}
         required={props.required}
+        className="px-4 py-3 rounded-lg bg-gray-200 border outline-none focus:border-blue-500 focus:bg-white transition-all"
       />
     </div>
   );
