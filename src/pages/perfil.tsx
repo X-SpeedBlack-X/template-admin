4f13160f17e44dbfed50adaede7e5833edc72068
isaque.s <isaque.error404@gmail.com>
2022-12-01 10:51:44 -0300

    feat: create new pages perfil

---
+++ src/pages/perfil.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/template/Layout";
+
+export default function Perfil() {
+  return (
+    <div>
+      <Layout title="Perfil" subtitle="Perfil do usuário">
+        <h1>Perfil config</h1>
+      </Layout>
+    </div>
+  );
+}
