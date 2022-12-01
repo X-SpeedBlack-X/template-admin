1f2e63b3debe486468d40c57d4396f6864d3b770
isaque.s <isaque.error404@gmail.com>
2022-12-01 10:51:06 -0300

    feat: add types

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -7,13 +7,15 @@ type AppContextProps = {
   changeTheme: () => void;
 };
 
-const AppContext = createContext<AppContextProps>({});
+const AppContext = createContext<AppContextProps>({} as AppContextProps);
 
 export function AppProvider(props) {
   const [theme, setTheme] = useState<Theme>("");
+
   function changeTheme() {
     setTheme(theme === "" ? "dark" : "");
   }
+
   return (
     <AppContext.Provider
       value={{
