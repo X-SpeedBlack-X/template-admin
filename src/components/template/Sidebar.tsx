04f25e6f4f1c391755da53d5eef6bffe233a3ba3
isaque.s <isaque.error404@gmail.com>
2022-12-05 12:41:36 -0300

    feat(sidebar): add logout

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,8 +1,10 @@
+import useAuth from "../../data/hook/useAuth";
 import { AdjustmentsIcon, BellIcon, HomeIcon, LogoutIcon } from "../icons";
 import { Logo } from "./Logo";
 import { MenuItem } from "./MenuItem";
 
 export function Sidebar() {
+  const { logout } = useAuth();
   return (
     <aside className="flex flex-col dark:bg-slate-900">
       <div className="flex items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500  to-purple-800  ">
@@ -17,10 +19,11 @@ export function Sidebar() {
 
       <ul>
         <MenuItem
-          onClick={() => console.log("sair")}
+          onClick={logout}
           text="Sair"
+          url="/login"
           icon={LogoutIcon}
-          className="text-red-600 hover:bg-red-400 hover:text-white cursor-pointer dark:bg-red-700 dark:hover:bg-red-500"
+          className="text-red-600 hover:bg-red-400 hover:text-white cursor-pointer  dark:hover:bg-red-700 "
         />
       </ul>
     </aside>
